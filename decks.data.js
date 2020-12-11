6c02266834cc09f21948ab7c95c5c7597a6e28f4
robokiaan <71754527+robokiaan@users.noreply.github.com>
2020-12-11 21:39:54 +0530

    Add files via upload

---
+++ decks.data.js
@@ -0,0 +1,10 @@
+var decks = [
+    "Addition Deck",
+    "Subraction Deck",
+    "Multiplication Deck",
+    "Division Deck",
+    "LCM Deck",
+    "HCF Deck",
+    "Exponents",
+    "Square Roots"
+];
